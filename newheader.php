044aa4d4e44f0e29bfab427a2b45cf58a639d159
8160988575 <harshchavda447@gmail.com>
2020-06-29 23:40:30 +0530

    newhadder

---
+++ newheader.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<meta charset="utf-8">
+			<title>Harmeety ||  harmeety partner  || harmeety restaurant ||  Harmeety offers || restaurant offers</title>
+			<meta name="description"  content="harmeety , Harmeety,hotel coupan app, dicount from restaurant for book table
+,restaurant offers site,restaurant offers in india,restaurant coupan in free,best offer in restaurant website
+,restaurant partner "/>
+
+ <meta name="keywords" content="harmeety ,harmeety web app,restaurant offers, harmeety restaurant offers
+ harmeety india,harmeet,best restaurant offers ,harmeet india ,harmeety offers,harmeety brothers,harmeety partner"/>
+
+  <meta name="author" content="S&Sn brothers"/>
+
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="newheadercss.css">
+       <link rel="stylesheet" type="text/css"   href="icon/css/all.css">
+</head>
+
+<body>
+
+
+ <div class="newheader">
+   <div class="newleft">
+         <i class="fas fa-angle-double-right" onclick="openslidefunction()" style="color:black;"></i>
+   </div>
+
+   <div class="newmiddle">
+       <ul style="list-style-type:none;">
+
+
+             <li class="notice">Name Of Assoc.</li>
+
+
+             <li class="search"><a href="mpagesearching.php"><i class="fa fa-search" aria-hidden="true" style="margin-right:5%;"></i>Search Here!!</li></a>
+
+      </ul>
+   </div>
+
+<div class="newlogo">
+
+
+           <img src="logo.png.jpg" alt="">
+</div>
+
+ </div>
+
+
+</body>
+
+</html>
